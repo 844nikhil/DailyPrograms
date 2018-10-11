d2fb2b3ae8673bea44d701b1c4e1d5b454648657
844nikhil <844nikhil@gmail.com>
2018-10-11 11:25:45 +0530

    Delete InsuranceInterface.java

--- src/main/java/com/capgemini/bank/InsuranceInterface.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini.bank;
-
-public interface InsuranceInterface {
-
-	String insuranceName(String insuranceName);
-	double insuranceAmount(double insuranceAmount);
-	boolean isLifeTime();
-}
